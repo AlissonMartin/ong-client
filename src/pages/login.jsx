b33316d4150a2ec3af1a0f37e77191a2fc42983d
Nicmar2000 <martinsnicolas2000@gmail.com>
2024-11-26 20:49:52 -0300

    correcao da tela de login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,21 +1,26 @@
+import { useState } from "react"
 import Header from "../components/header.jsx"
+import LoginOng from "../components/loginOng.jsx"
 import LoginUsu from "../components/loginUsu.jsx"
 
 function Login(){
+    const [usuario , setState] = useState(<LoginUsu/>)
     return(
         <div>
             <Header />
             <div className="flex justify-center p-10">
                 <div className=" bg-slate-400 rounded-sm w-[500px] p-10">
                     <div className="bg-lime-600 text-center p-4 rounded-md">
-                        <button>Usuario</button>
+                        <button onClick={() => {setState(<LoginUsu/>)}}>Usuario</button>
                     </div><br/>
                     <div className="bg-lime-600 text-center p-4 rounded-md">
-                        <button>Ong</button>
+                        <button onClick={() => {setState(<LoginOng/>)}}>Ong</button>
                     </div>
                 </div>
             </div>
-            <LoginUsu/>
+            <div>
+                {usuario}
+            </div>
         </div>
     )
 }
