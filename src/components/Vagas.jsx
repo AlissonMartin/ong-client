0da9cc624643145b489be6b5080a18a96de72687
Nicmar2000 <martinsnicolas2000@gmail.com>
2025-05-28 15:59:23 -0300

    Finalização da pagina da ong e comeco dos componentes de vaga e postagem

---
+++ src/components/Vagas.jsx
@@ -0,0 +1,19 @@
+function Vagas() {
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-4 pb-2">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Cozinheiro</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-success">Candidatar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Vagas
--- src/pages/ong/OngToUsu.jsx
+++ src/pages/ong/OngToUsu.jsx
@@ -1,8 +1,11 @@
 import HeaderLog from "../../components/HeaderLog";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Vagas from "../../components/Vagas";
+import Post from "../../components/Post";
 
 function OngToUsu() {
-    const navigate = useNavigate();
+    const [isUser, setIsUser] = useState(true);
+
 
     return (
         <div>
@@ -21,7 +24,25 @@ function OngToUsu() {
                     <div className="col-12 pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer imperdiet lorem ex, id malesuada eros dignissim id. Morbi vehicula libero augue, vel sagittis nisl sodales eu. In vitae arcu dolor. Donec ut quam arcu. Maecenas vestibulum quam vel leo lacinia, lacinia euismod dui porta. Aliquam tempus finibus turpis sit amet elementum. Vivamus eu vehicula velit. Nulla quis sodales nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent ac auctor tellus. Praesent rutrum odio in dui luctus, a aliquam sem tempus. Proin dictum et dui in viverra. Mauris porttitor ex sapien, vitae auctor massa suscipit ac. Suspendisse at aliquet enim.</div>
                 </div>
             </div>
-
+            <div className="d-grid gap-2 d-md-flex justify-content-md-center pt-5 pb-3">
+                <button
+                    className="btn btn-success fw-semibold"
+                    onClick={() => setIsUser(true)}
+                    disabled={isUser === true}
+                >
+                    Vagas
+                </button>
+                <button
+                    className="btn btn-success fw-semibold"
+                    onClick={() => setIsUser(false)}
+                    disabled={isUser === false}
+                >
+                    Postagens
+                </button>
+            </div>
+            <div className="d-flex justify-content-center">
+                {isUser ? <Vagas /> : <Post />}
+            </div>
         </div>
     );
 }
